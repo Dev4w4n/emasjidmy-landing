709783f01cff293d2dc64cbc1fd254b5dcdb5bcb
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2024-03-24 01:41:49 +0800

    Rewrite the teams component to fix the grid alignment

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,24 +1,25 @@
 export const metadata = {
-  title: 'E-Masjid.My',
-  description: 'E-Masjid.My: Sistem Masjid Untuk Semua. Ihsan dari warga Github.',
-}
+  title: "E-Masjid.My",
+  description:
+    "E-Masjid.My: Sistem Masjid Untuk Semua. Ihsan dari warga Github.",
+};
 
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import FeaturesBlocks from '@/components/features-blocks'
-import Testimonials from '@/components/testimonials'
-import Newsletter from '@/components/newsletter'
-import Team from '@/components/team'
+import Hero from "@/components/hero";
+import Features from "@/components/features";
+import FeaturesBlocks from "@/components/features-blocks";
+import Testimonials from "@/components/testimonials";
+import Newsletter from "@/components/newsletter";
+import Teams from "@/components/teams";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Team />
+      <Teams />
       <Features />
       <FeaturesBlocks />
       <Testimonials />
       <Newsletter />
     </>
-  )
+  );
 }
