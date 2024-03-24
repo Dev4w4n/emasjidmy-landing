a5df79131c83435550e606b0a5b9cb708e0dd47b
NONAME <mr.zexr2010@gmail.com>
2024-03-25 00:03:34 +0800

    Update banner.tsx
    - add simple bounce on desktop screen

--- components/banner.tsx
+++ components/banner.tsx
@@ -8,10 +8,10 @@ export default function Banner() {
 
   return (
     <>
-      {bannerOpen && (
+      {bannerOpen ? (
         <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-50">
-          <div className="bg-slate-800 text-slate-50 text-sm p-3 md:rounded shadow-lg flex justify-between">
-            <div className='text-slate-500 inline-flex'><a className="font-medium hover:underline text-emerald-400" href="https://wa.me/+60124062988" target='_blank' rel="noreferrer">Ada soalan? Whatssap kami.</a></div>
+          <div className="bg-slate-800 text-slate-50 text-sm p-3 md:rounded shadow-lg flex justify-between lg:animate-bounce">
+            <div className='text-slate-500 inline-flex'><a className="font-medium hover:underline text-emerald-400" href="https://wa.me/60124062988" target='_blank' rel="noreferrer">Ada soalan? WhatsApp kami.</a></div>
             <button className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-gray-700" onClick={() => setBannerOpen(false)}>
               <span className="sr-only">Close</span>
               <svg className="w-4 h-4 shrink-0 fill-current" viewBox="0 0 16 16">
@@ -20,7 +20,7 @@ export default function Banner() {
             </button>
           </div>
         </div>
-      )}
+      ): null}
     </>
   )
 }
