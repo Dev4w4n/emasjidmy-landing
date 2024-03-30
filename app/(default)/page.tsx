6544791aa2e996c903eba075fb9a9ad589e9a0e6
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2024-03-30 14:50:49 +0800

    Revert

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,26 +1,15 @@
+export const metadata = {
+  title: "E-Masjid.My",
+  description:
+    "E-Masjid.My: Sistem Masjid Untuk Semua. Ihsan dari warga Github.",
+};
+
 import Hero from "@/components/hero";
 import Features from "@/components/features";
 import FeaturesBlocks from "@/components/features-blocks";
 import Testimonials from "@/components/testimonials";
 import Newsletter from "@/components/newsletter";
 import Teams from "@/components/teams";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "E-Masjid.My",
-  description:
-    "E-Masjid.My: Sistem Masjid Untuk Semua. Ihsan dari warga Github.",
-  openGraph: {
-    images: {
-      url: "https://images.unsplash.com/photo-1682687982183-c2937a74257c?q=80&w=3571&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  },
-  twitter: {
-    images: {
-      url: "https://images.unsplash.com/photo-1682687982183-c2937a74257c?q=80&w=3571&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  },
-};
 
 export default function Home() {
   return (
